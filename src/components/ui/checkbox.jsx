29db5e8ca1bb1b9205c6147f2169e08f0d5cfd07
Marcin Micek <help@handypioneers.com>
2025-11-11 18:54:52 +0000

    File changes

--- src/components/ui/checkbox.jsx
+++ src/components/ui/checkbox.jsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { Check } from "lucide-react"
@@ -5,7 +6,7 @@ import { Check } from "lucide-react"
 const Checkbox = React.forwardRef(({ className = "", ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
-    className={`peer h-4 w-4 shrink-0 rounded-sm border border-gray-300 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gray-900 data-[state=checked]:text-white ${className}`}
+    className={`peer h-4 w-4 min-h-[16px] min-w-[16px] max-h-[16px] max-w-[16px] shrink-0 rounded-sm border border-gray-300 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gray-900 data-[state=checked]:text-white ${className}`}
     {...props}
   >
     <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current">
