b3cf26699a42f98f9cd45e13e0d6fb232a6ea85a
Marcin Micek <help@handypioneers.com>
2025-11-23 02:35:49 +0000

    File changes

--- src/components/demo/PortfolioDistribution.jsx
+++ src/components/demo/PortfolioDistribution.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createPageUrl } from '@/utils';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { Progress } from '@/components/ui/progress';
